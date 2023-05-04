e7642edfd5cc696265b294e55f7ae7b6d27019c0
Yusi0114 <132541872+Yusi0114@users.noreply.github.com>
2023-05-04 16:20:06 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,9 +15,20 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('/H9NE43vOP43+/ChC1/mBFyVXdRQqANXBhzNjXxTmcePo42k4AINP5akLybyEEi1knYA7wca56rwkVrPVFKJIp2BRWQIfxz3QsCfhIt+im9+XPxfEm1kgtiGOMDDfYqMMtULOXsNUGikVdl+Y9SFqwdB04t89/1O/w1cDnyilFU=') # your access token
 webhook_handler = WebhookHandler('26016dfbdd5f59b4faefded8639c27c0') # your channel secret
 
+# 設定 OpenAI API 密鑰
+openai.api_key = os.environ["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"]
+# openai.api_key = "OPENAI_API_KEY"
+
+# 設定 GPT-3.5 模型的檢索引擎
+model_engine = "text-davinci-003"
+
+# 設定生成的文本長度
+output_length = 300
+
+
 @app.route("/")
 def home():
-    return "LINE bot API Server is running"
+    return "LINE BOT API Server is running."
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,9 +51,20 @@ def callback():
 
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    # 使用 GPT-3.5 模型生成文本
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=event.message.text,
+        max_tokens=output_length,
+    )
+
+    # 取得生成的文本
+    output_text = response.choices[0].text.strip()
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=output_text))
 
 
 if __name__ == "__main__":
